59a4926dd729e16586ba10fb80c95855225debad
Tanner Stirrat <tstirrat@gmail.com>
2019-01-16 13:06:50 -0800

    minimal configuratain

--- webpack.config.js
+++ webpack.config.js
@@ -2,32 +2,15 @@ const webpack = require('webpack');
 
 module.exports = {
     entry: "./src/entry",
-    target: 'web',
     output: {
         path: __dirname + "/dist",
         filename: "bundle.js",
         publicPath: '/'
     },
-    resolve: {
-      extensions: ['.js']
-    },
     plugins: [
-      new webpack.DefinePlugin({
-        __FILE_FORMAT__: JSON.stringify('.json'),
-        __SUBDIR__: JSON.stringify('bar')
-      })
+        new webpack.DefinePlugin({
+            __FOO__: JSON.stringify('foo'),
+            __BAR__: JSON.stringify('bar')
+        })
     ],
-    module: {
-      rules: [
-        {
-          test: /\.(txt|json)(\?.*$|$)/,
-          use:{
-            loader: 'file-loader',
-            options: {
-              name: 'assets/[name].[ext]'
-            }
-          }
-        }
-      ]
-  }
 }
